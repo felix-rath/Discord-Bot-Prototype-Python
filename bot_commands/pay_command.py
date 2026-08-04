1994797e10df0a1d7e5565e7ec7a004b38da13b4
felix-rath <felix-rathmann@gmx.de>
2026-08-04 19:29:11 +0200

    Forgot pay command blance remove/add.
    Signed-off-by: felix-rath <felix-rathmann@gmx.de>

--- bot_commands/pay_command.py
+++ bot_commands/pay_command.py
@@ -1,4 +1,5 @@
 from bot_commands.base_command import Command
+import user_data.economy_manager as economy_manager
 
 class PayCommand(Command):
 
@@ -17,8 +18,19 @@ class PayCommand(Command):
             await message.channel.send("❌ Der **Betrag** muss eine Zahl sein.")
             return
 
+        if amount <= 0:
+            await message.channel.send("❌ Der Betrag muss größer als **0 Coins** sein.")
+            return
+        
         sender = message.author
         receiver = message.mentions[0]
+
+        if await economy_manager.remove_balance(sender.id, amount) is None:
+            await message.channel.send(
+            f"❌ **{sender.display_name}**, du hast nicht genug **Coins**! ")
+            return
+
+        await economy_manager.add_balance(receiver.id, amount)
 
         await message.channel.send(
             f"💸 **{sender.display_name}** hat **{receiver.display_name}** "
